669a591296ab8371090dfedef3dbb5041ee2e72a
saharan-m <mayanksaharan18@gmail.com>
2020-12-20 23:29:16 +0530

    cookies

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis'
+import { MyContext } from './types';
+
 const main = async()=>{
     const orm = await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
@@ -19,12 +24,33 @@ const main = async()=>{
 
     // const posts = await orm.em.find(Post,{});
     // console.log(posts);
+    const RedisStore = connectRedis(session);
+    const redisClient = redis.createClient();
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch:true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+                httpOnly: true,
+                secure: __prod__,
+                sameSite:'lax',
+            },
+            saveUninitialized:false,
+            secret: "dkjadkadnkla",
+            resave: false,
+        })
+    )
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers:[HelloResolver,PostResolver,UserResolver],
             validate: false,
         }),
-        context: () => ({em: orm.em})
+        context: ({ req, res }) : MyContext => ({em: orm.em,req,res})
     });
     apolloServer.applyMiddleware({app});
     app.listen(3000,()=>{
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "../types";
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from 'argon2'
 
@@ -31,10 +31,20 @@ class UserResponse{
 
 @Resolver()
 export class UserResolver{
+    @Query(()=> User, {nullable: true})
+    async me(
+        @Ctx(){ req ,em} : MyContext
+    ){
+        if(!req.session.userId){
+            return null;
+        }
+        const user = await em.findOne(User, {id: req.session.userId});
+        return user;
+    }
     @Mutation(()=> UserResponse)
     async register(
        @Arg("options") options: UsernamePasswordInput,
-       @Ctx(){em}:MyContext
+       @Ctx(){em, req}:MyContext
     ): Promise<UserResponse>{
         if(options.username.length <=2){
             return {
@@ -69,12 +79,14 @@ export class UserResolver{
             }
             }
         }
+        //keep them logged in
+        req.session.userId = user.id;
         return {user};
     }
     @Mutation(()=> UserResponse)
     async login(
        @Arg('options') options: UsernamePasswordInput,
-       @Ctx(){em}:MyContext
+       @Ctx(){em,req}:MyContext
     ):Promise<UserResponse>{
         const  user = await em.findOne(
             User,{username: options.username}
@@ -96,7 +108,7 @@ export class UserResolver{
                 }],
             }
         }
-
+        req.session.userId = user.id;
 
         return {
             user: user
